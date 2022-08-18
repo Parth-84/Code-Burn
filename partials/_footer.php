25b4a3919fe1f101f285524f38171968f659a2ac
Parth <parthparmar25.pp@gmail.com>
2022-08-18 19:26:54 +0530

    Updated Footer

--- partials/_footer.php
+++ partials/_footer.php
@@ -2,7 +2,7 @@
 <!-- <footer class="footer bg-dark my-0 py-2"> -->
 
   <!-- Copyright -->
-  <div class="footerE bg-dark my-0 py-2 footer_con">© 2021 Copyright:
+  <div class="footerE bg-dark my-0 py-2 footer_con">© 2022 Copyright:
     <a href="#">Code Burn| By Parth Parmar</a>
   </div>
   <!-- Copyright -->
